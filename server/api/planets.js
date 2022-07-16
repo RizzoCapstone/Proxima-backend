818e77957405f9b10161a4252980cb4592f89a10
tylen101 <98216120+tylen101@users.noreply.github.com>
2022-07-16 18:24:32 -0400

    Update planets.js
    changed to fetch all planets of that type

--- server/api/planets.js
+++ server/api/planets.js
@@ -40,48 +40,30 @@ router.get('/:type', async (req, res, next) => {
         planets = await System.findAll({
           where: {
             planetMassE: { [Op.between]: ['0', '2'] },
-          },
-          limit: 20,
+          }
         });
         break;
       case type === 'superearth':
         planets = await System.findAll({
           where: {
             planetMassE: { [Op.between]: ['2', '10'] },
-          },
-          limit: 20,
+          }
         });
         break;
       case type === 'neptuntunian':
         planets = await System.findAll({
           where: {
             planetMassE: { [Op.between]: ['10', '50'] },
-          },
-          limit: 20,
+          }
         });
         break;
       case type === 'gasgiant':
         planets = await System.findAll({
           where: {
             planetMassE: { [Op.between]: ['50', '5000'] },
-          },
-          limit: 20,
+          }
         });
         break;
-
-      //     // case mass >= 0.5 && mass <= 2:
-      //     //   if (temp >= 275 && temp <= 375) return "earthLike";
-      //     //   return "rocky";
-      //     // case mass > 2 && mass <= 10:
-      //     //   if (temp >= 275 && temp <= 375) return "earthLike";
-      //     //   return "rocky";
-      //     // case mass > 10 && mass <= 50:
-      //     //   if (temp >= 273) return "gas";
-      //     //   return "icy";
-      //     // //https://exoplanets.nasa.gov/what-is-an-exoplanet/planet-types/neptune-like/
-      //     // case mass > 50 && mass <= 5000:
-      //     //   return "gas";
-      //     //Jovian planets tend to have very thick atmospheres consisting of hydrogen, helium, methane, and ammonia
       default:
         break;
     }
